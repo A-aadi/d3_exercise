e259b50027b35ec20131e53814397d3f833c07b9
adarsh <adarshsa@thoughtworks.com>
2016-11-05 15:19:11 +0530

    completed bar chart, now its working fine

--- ranodm_number/public/js/barChartD3.js
+++ ranodm_number/public/js/barChartD3.js
@@ -1,75 +1,53 @@
+const LIMIT = 1000;
+var lastSequenceProvided = 0;
 
-var randomBlue = function () {
-    return "rgb("+Math.floor(Math.random() * 100)+", "+ Math.floor(Math.random() * 255)+", 255)";
+var getBlueColor = function (d) {
+    return "rgb("+(d * 100)%50+", " + ((d * 100)%255) + ", 255)";
 };
 
-var id = 0;
-
-var drawBars = function (data) {
-    debugger;
-  var bar = d3.select("#container").selectAll("div")
-      .data(data, function (d) {
-          return d.id;
-      })
-      .enter()
-      .append("div")
-      .classed("bar", true)
-      .style("width", function (d) {
-          return d.number + "px";
-      })
-      .style("background",function(){
-          return randomBlue();
-      })
-      .text(function (d) {
-          return d.number;
-      });
-var oldBar = bar;
-    bar.data(data, function (d) {
-       return d.id;
-    }).style("width",function (d) {
-        return d.number +'px';
-        })
-        .text(function (d) {
-            return d;
-        });
-    var newBar = bar;
-    bar.exit().remove();
+var getSequence = function () {
+    return lastSequenceProvided++;
 };
 
-var updateBar = function (data) {
-    d3.selectAll(".bar").data(data, function () {
-        return id++;
-    })
-        .style("width",function (d) {
-            return d.number +'px';
-        })
-        .text(function (d) {
-            return d.number;
-        });
-        // .style("background", function () {
-        //     return randomBlue();
-        // })
-};
 
-var get_random_numbers = function(){
-    var numbers =[];
-    for(var i = 0; i< 10;i++){
-        numbers.push({"number":Math.floor(Math.random() * 800) + 200, "id": id++});
+
+var randomNumbers = function () {
+    var numbers = [];
+    for (var i = 0; i < 10; i++) {
+        numbers.push(Math.round(Math.random() * LIMIT));
     }
     return numbers;
 };
 
-var createBarChart = function(){
-    var numbers = get_random_numbers();
-    drawBars(numbers);
-    setInterval(function () {
-        var value = Math.floor(Math.random() * 800) + 200;
-        numbers.shift();
-        numbers.push(value);
-        drawBars(numbers);
-        // updateBar(numbers);
-    },900)
+var generateBars = function (data) {
+    var divs = d3.select('#container').selectAll('div').data(data, function () {
+        return getSequence();
+    });
+
+    divs.enter()
+        .append('div')
+        .classed('bar', true)
+        .style('width', function (d) {
+            return d + 'px'
+        })
+        .style('background', function (d) {
+            return getBlueColor(d);
+        })
+        .text(function (d) {return d});
+
+    divs.exit().remove();
 };
 
-window.onload = createBarChart;
 
+var execute = function () {
+    var data = randomNumbers();
+    setInterval(function () {
+        var value = Math.round(Math.random() * LIMIT);
+        generateBars(data);
+        data.shift();
+        data.push(value);
+    }, 200);
+};
+
+
+window.onload = execute;
