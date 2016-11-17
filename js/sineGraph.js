0e3b3872c119abb1d9830da91cd1a1aa9cccd113
adarsh <adarshsa@thoughtworks.com>
2016-11-17 20:26:17 +0530

    implemented interpolate in sine graph

--- js/sineGraph.js
+++ js/sineGraph.js
@@ -6,8 +6,34 @@ const INNER_WIDTH = WIDTH - 2 * MARGIN;
 const INNER_HEIGHT = HEIGHT - 2 * MARGIN;
 
 
-var createLineGraph = function () {
+var createChart = function () {
 
+    var d = [{"curveLinear":d3.curveLinear,"curveTitle":"curveLinear"},
+        {"curveStep":d3.curveStep,"curveTitle":"curveStep"},
+        {"curveStepBefore":d3.curveStepBefore,"curveTitle":"curveStepBefore"},
+        {"curveStepAfter":d3.curveStepAfter,"curveTitle":"curveStepAfter"},
+        {"curveBasis":d3.curveBasis,"curveTitle":"curveBasis"},
+        {"curveCardinal":d3.curveCardinal,"curveTitle":"curveCardinal"},
+        {"curveMonotoneX":d3.curveMonotoneX,"curveTitle":"curveMonotoneX"},
+        {"curveCatmullRom":d3.curveCatmullRom,"curveTitle":"curveCatmullRom"}];
+
+    createLineGraph();
+    createButtons(d);
+};
+
+var createLineGraph = function (interpolate) {
+    if (!document.getElementById("container").innerHTML == "" || null){
+        d3.select("svg").remove();
+    }
+    var interpolateObj = {"curveLinear":d3.curveLinear, "curveStep":d3.curveStep, "curveStepBefore":d3.curveStepBefore, "curveStepAfter":d3.curveStepAfter,
+        "curveBasis":d3.curveBasis, "curveCardinal":d3.curveCardinal, "curveMonotoneX":d3.curveMonotoneX, "curveCatmullRom":d3.curveCatmullRom};
+
+    if(!interpolate){
+        interpolate = "curveLinear";
+    }
+
+    document.getElementById("graphInterpolate").innerHTML = interpolate;
+    
     var data = [
         {x: 0, y: 5},
         {x: 1, y: 9},
@@ -24,11 +50,8 @@ var createLineGraph = function () {
     var svg = d3.select("#container")
         .append("svg")
         .attr("width",WIDTH)
-        .attr("height", HEIGHT);
-
-    var translate = function(x, y){
-        return "translate("+x+","+y+")";
-    };
+        .attr("height", HEIGHT)
+        .text("ggmmmmm")
 
     var xScale = d3.scaleLinear().domain([0, 1]).range([0,INNER_WIDTH]);
     var yScale = d3.scaleLinear().domain([1, 0]).range([0,INNER_HEIGHT]);
@@ -49,20 +72,23 @@ var createLineGraph = function () {
 
     var line = d3.line()
         .x(function (d) {return xScale(d.x/10) ; })
-        .y(function (d) {return yScale(d.y/10) ; });
+        .y(function (d) {return yScale(d.y/10) ; })
+        .curve(interpolateObj[interpolate]);
 
     g.append('path')
         .attr('d', line(data))
-        .attr("stroke","steelblue")
+        .attr("stroke","red")
         .attr("fill", "none");
 
     var sinLine = d3.line()
         .x(function (d) {return xScale(d.x/10) ; })
-        .y(function (d) {return yScale((Math.sin(d.x))/10 + 0.5)});
+        .y(function (d) {return yScale((Math.sin(d.x))/10 + 0.5)})
+        .curve(interpolateObj[interpolate]);
+
 
     g.append('path')
         .attr('d', sinLine(data))
-        .attr("stroke","steelblue")
+        .attr("stroke","blue")
         .attr("fill", "none");
 
     g.selectAll('circle').data(data)
@@ -76,7 +102,7 @@ var createLineGraph = function () {
             return yScale(d.y/10);
         })
         .attr("fill", "white")
-        .attr("stroke", "steelblue");
+        .attr("stroke", "blue");
 
     g.selectAll('circleForSin').data(data)
         .enter()
@@ -93,4 +119,24 @@ var createLineGraph = function () {
 
 };
 
-window.onload = createLineGraph ;
+var translate = function(x, y){
+    return "translate("+x+","+y+")";
+};
+
+var handleOnclickEvents = function () {
+    createLineGraph(this.innerHTML)
+};
+
+var createButtons = function (d) {
+    var container = document.getElementById("button_container");
+    for(var i=0; i<d.length; i++){
+        var button = document.createElement("BUTTON");
+        button.textContent = d[i].curveTitle;
+        button.style.margin = '15px';
+        button.className = "curveButton";
+        button.onclick = handleOnclickEvents;
+        container.appendChild(button)
+    }
+};
+
+window.onload = createChart ;
